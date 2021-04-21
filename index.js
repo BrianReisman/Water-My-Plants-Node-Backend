d22c82bd1edbef2a5437ca1b7e68621f5b8965c1
BrianReisman <brianreisman@protonmail.com>
2021-04-21 11:46:37 -0400

    add dotenv package and configure .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const server = require('./api/server');
 
-const port = 5500
+const port = process.env.PORT;
 
 server.listen(port, ()=>{
   console.log(`\n ***server is listening on port: ${port}*** \n`)
