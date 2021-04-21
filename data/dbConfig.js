981bd95cc52a3101941d26588b753d25ac70b9aa
BrianReisman <brianreisman@protonmail.com>
2021-04-21 14:58:28 -0400

    knex config for deployment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex')
 
 const knexfile = require('../knexfile')
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 
 module.exports = knex(knexfile[environment])
