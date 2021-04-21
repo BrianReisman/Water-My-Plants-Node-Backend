1eb8b13939c28471510372f417e033f32e07b368
BrianReisman <brianreisman@protonmail.com>
2021-04-21 14:24:37 -0400

    knex config for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,38 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
-    migrations: { directory: './data/migrations'},
-    seeds: {directory: './data/seeds'},
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
     connection: {
-      filename: './data/user.db3'
-    }
+      filename: "./data/user.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
   },
+
   production: {
-    client: 'sqlite3',
+    client: "sqlite3",
+    useNullAsDefault: true,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+    pool: {
+      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+    },
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: "./data/waterMyPlantsDBProduction.db3",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-  
+  },
+
   // staging: {
   //   client: 'postgresql',
   //   connection: {
@@ -42,6 +48,4 @@ module.exports = {
   //     tableName: 'knex_migrations'
   //   }
   // },
-
-
 };
